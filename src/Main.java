56fdb12be5eb7bdce6eedffda13c63dbf1c77515
Rohit Retnakaran <rohitretnakaran@gmail.com>
2024-03-07 15:07:15 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,4 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
-    //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
 }
